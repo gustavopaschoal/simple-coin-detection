8f1bcde5be79405423edc1102249cd11bc20e166
gustavopaschoal <69567081+gustavopaschoal@users.noreply.github.com>
2020-10-04 16:22:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 The build/compilations setup
 
 >> pip install -r requirements.txt
+>> python setup.py install
+
 """
 
 from setuptools import setup
 
 setup(
     name='simple-coin-detection',
-    version='1.0',
+    version='1.0.0',
     description='Simple coin detection and counting using OpenCV',
+    packages=['coincounter'],
     author='Gustavo Paschoal',
     author_email='gpmiranda93@gmail.com',
-    url='https://github.com/gustavopaschoal/simple-coin-detection',
-	packages=['coincounter']
+    url='https://github.com/gustavopaschoal/simple-coin-detection'	
 )
